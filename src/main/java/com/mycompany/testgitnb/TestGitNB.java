a45ddf63e07245b9f9e4b813181ddb6446467eb6
Lukas Sumkauskas <C00263149@itcarlow.ie>
2023-04-08 18:40:02 -0700

    Edit code

--- src/main/java/com/mycompany/testgitnb/TestGitNB.java
+++ src/main/java/com/mycompany/testgitnb/TestGitNB.java
@@ -12,6 +12,6 @@ package com.mycompany.testgitnb;
 public class TestGitNB {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello from main branch!");
     }
 }
